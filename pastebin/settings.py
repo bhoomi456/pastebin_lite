3663765adf95bb966e258cc03ba309ec941aec2a
rishi-jain1 <rishi_jain@samyaktechlabs.com>
2026-01-28 18:38:14 +0530

    Prepare app for Render deployment

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4@eau)3jdivy_&3h-@xkp8_d&nsp@p4v%f7oz87807#n@h+@oy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "staticfiles"
